4a984ded804f1c46c5d2557f059fec949c2ef2a9
LuckTheGamers <thegamers056@gmail.com>
2023-06-06 19:40:34 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Sobre
+
+ Esse repositório é sobre o vídeo da Coffstack que ensina a utilizar a ferramenta REDUX.
+
+## Aprendizado
+
+ 📍 Funcionamento do Redux
+ 📍 Algumas perguntas de entrevistas de emprego
+ 📍 Jeito antigo e o novo com o Redux Toolkit
+ 📍 Sobre tipagem utilizando o Typescript
+
+### 🔗 Links:
+
+ 📍 Aonde foi testado 👉 [clique aqui](api-test-swart.vercel.app)
+
+ 📍 Link do vídeo 👉 [clique aqui](https://www.youtube.com/watch?v=HGMd3dbB-lM)
---
+++ src/App.tsx
@@ -0,0 +1,77 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+//novo metodo de importar as actions
+import { increment, incrementAmount } from './assets/redux/newWay/features/counter/counter-slice';
+
+// antigamente se importava o connect para o jeito oldSchool 
+import { useDispatch, useSelector } from 'react-redux/es/exports' // useSelector hook padrao pra ter o estado
+//useDispatch hook para trocar o estado
+import { useAppSelector } from './assets/redux/newWay/customHooks'; // hook customizavel do useSelector
+
+// antigamente se passava os valores do connect ex map state to props para a função, assim ficava function App({ count, increment, incrementAmount })
+function App() {
+
+  // const count = useAppSelector((state) => state.counter.value); maneira sem o hook customizavel, que so tem a tipagem do estado como diferença
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  function handleOnClick(){
+    // increment(); versão antiga oldscholl
+    // dispatch({type: 'counter/increment'}); maneira old new 
+    dispatch(increment()); //jeito novo toolkit
+  }
+
+  function handleOnClickAmount(){
+    // incrementAmount(5); versão antiga oldscholl
+    // dispatch({type: 'counter/incrementAmount', payload: 5}); maneira old new
+    dispatch(incrementAmount(5)); //jeito novo toolkit
+  } 
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleOnClick}>
+          count is {count}
+        </button>
+        <button onClick={handleOnClickAmount}>
+          Increment Amount
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App;
+
+// MANEIRA ANTIGA (OLDSCHOOL)
+// const mapStateToProps = (state) => {
+//   return {
+//     count: state.counter.value,
+//   };
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({type: 'counter/increment'}),
+//     incrementAmount: (amount) => dispatch({type: 'counter/incrementAmount', payload: amount}),
+//   };
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(App);
